622f57c1999b455bcc0258490610ae0ceaf8970b
ead <ead@academico.local>
2024-09-25 21:47:48 -0300

    Exercício 2.2

--- livrojs/cap02/js/exe2_1.js
+++ livrojs/cap02/js/exe2_1.js
@@ -3,8 +3,10 @@ const frm = document.querySelector("form");
 const resp = document.querySelector("h3");
 
 //cria um "ouvinte" de evento, acionado quando o botão submit for clicado
-frm.addEventListener("submit", (e)=>{
-    const nome = frm.inNome.value; //obtem o nome digitado no form
-    resp.innerText = `Olá ${nome}`; //exibe a resposta do programa
-    e.preventDefault(); //vai evitar que o form envie os dados para destino.php
-});
+frm.addEventListener("submit", executar);
+
+function executar(evento) {
+    const nome = frm.inNome.value;
+    resp.innerHTML = `Olá <i>${nome}</i>`;
+    evento.preventDefault();
+}
---
+++ livrojs/cap02/js/exe2_2.js
@@ -0,0 +1,16 @@
+const frm = document.querySelector("#filmeForm");
+const resp1 = document.querySelector("h3");
+const resp2 = document.querySelector("h4");
+
+frm.addEventListener("submit", (e)=>{
+    //entrada de dados
+    const titulo = frm.inTitulo.value;
+    const duracao = frm.inDuracao.value;
+    //processamento
+    const horas = Math.floor(duracao/60);
+    const minutos = duracao%60;
+    //saídas
+    resp1.innerText = titulo;
+    resp2.innerText = `${horas} hora(s) e ${minutos} minuto(s)`;
+    e.preventDefault();
+});
