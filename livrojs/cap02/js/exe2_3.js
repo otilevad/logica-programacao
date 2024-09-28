b448653cece82a97bf41da80ad00f20e5f8a8f55
ead <ead@academico.local>
2024-09-27 21:49:07 -0300

    2.3

---
+++ livrojs/cap02/js/exe2_3.js
@@ -0,0 +1,18 @@
+const frm = document.querySelector("#carroForm");
+const resp1 = document.querySelector("#outTitle");
+const resp2 = document.querySelector("#outEntrada");
+const resp3 = document.querySelector("#outParcelas");
+
+frm.addEventListener("submit", (e)=>{
+    //entrada de dados
+    const titulo = frm.inVeiculo.value;
+    const preco = frm.inPreco.value;
+    //processamento
+    const entrada = preco*.5;
+    const parcelas = (preco-entrada)/12;
+    //saídas
+    resp1.innerText = `Promoção: ${titulo}`;
+    resp2.innerText = `Entrada de R$: ${entrada.toFixed(2)}`;
+    resp3.innerText = `+12x de R$ ${parcelas.toFixed(2)}`;
+    e.preventDefault();
+});
