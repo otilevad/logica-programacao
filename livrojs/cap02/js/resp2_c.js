5ec95f0bf37f696c4f93fbcc83e6650e167b751e
otilevad <arthurdavelvb@gmail.com>
2024-09-28 02:26:12 -0300

    Resp 2.C

--- livrojs/cap02/js/resp2_c.js
+++ livrojs/cap02/js/resp2_c.js
@@ -3,13 +3,10 @@ const resp1 = document.querySelector("#outPromo");
 const resp2 = document.querySelector("#outTerceiro");
 
 frm.addEventListener("submit", (e)=>{
-    //entrada de dados
     const produto = frm.inProduto.value;
     const preco = Number(frm.inPreco.value).toFixed(2);
-    //processamento
     const valorTerceiro = preco*.5;
     const valorTotal = preco*2.5;
-    //saídas
     resp1.innerText = `${produto} - Promoção: Leve 3 por R$: ${valorTotal}`;
     resp2.innerText = `O 3° produto custa apenas R$: ${valorTerceiro}`;
     e.preventDefault();
