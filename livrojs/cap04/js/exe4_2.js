ba1a2c60851fe34185589acd6aca2ccd242ce7a5
otilevad <arthurdavelvb@gmail.com>
2024-10-02 21:43:42 -0300

    Exercício 4.2

---
+++ livrojs/cap04/js/exe4_2.js
@@ -0,0 +1,8 @@
+function calcular(){
+    const nome = document.getElementById("inNome").value;
+    const sexo = document.getElementById("inMasculino").checked;
+    const altura = Number(document.getElementById("inAltura").value);
+    const peso = sexo ? 22*Math.pow(altura,2) : 21*Math.pow(altura,2);
+    document.querySelector("#outPeso").innerText = `${nome}: Seu peso ideal é ${peso.toFixed(2)} kg`;
+    return false; //impede o refresh
+}
